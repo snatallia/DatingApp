a78d965a3219bcd00a295757643fb817c27ae3a8
snatallia <you@example.com>
2021-09-02 17:34:43 +0300

    Added register component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,12 +11,12 @@ import { AccountService } from './_services/account.service';
 
 export class AppComponent implements OnInit {
   title = 'The Dating App Page';
-  users:any;
 
-  constructor(private http: HttpClient, private accountService: AccountService){}
+
+  constructor(private accountService: AccountService){}
 
   ngOnInit()  {
-    this.getUsers();
+    //this.getUsers();
     this.setCurrentUser();
   }
 
@@ -25,11 +25,11 @@ export class AppComponent implements OnInit {
     this.accountService.setCurrentUser(user);
   }
 
-  getUsers(){
+ /* getUsers(){
     this.http.get('https://localhost:5001/api/users').subscribe(response => {
       this.users = response;
     }, error =>{
       console.log(error);
     })
-  }
+  }*/
 }
