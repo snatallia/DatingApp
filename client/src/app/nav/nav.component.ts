82842c1a825a58c958e091cf818f0f613dd74ef5
snatallia <you@example.com>
2021-09-02 09:54:57 +0300

    Added accountService

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,13 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   model :any = {}
-  constructor() { }
+  loggedIn: boolean = false;
+
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
-  login() {
-    console.log(this.model);
+  login() { 
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+      this.loggedIn = true;
+    }, error =>{
+      console.log(error);
+    })
   }
 
 }
